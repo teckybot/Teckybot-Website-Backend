99f28d5903b7361c3760a9235089eea4f0d1494e
B.K.S.S.S MALLIK <sssmallik143@gmail.com>
2025-08-07 18:09:37 +0530

    configured for info mail

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import morgan from 'morgan'; // ✅ Import morgan
+import morgan from 'morgan'; 
 
 import connectDB from './config/db.js';
 import contactRoutes from './routes/contactRoutes.js';
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(morgan('dev')); // ✅ Use morgan in 'dev' mode
+app.use(morgan('dev')); 
 
 // Routes
 app.use('/api', contactRoutes);
--- src/services/emailService.js
+++ src/services/emailService.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtppro.zoho.in',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
